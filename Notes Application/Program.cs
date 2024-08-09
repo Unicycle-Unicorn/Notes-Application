2460e04ee6aedd6a9e13a2a6bf7f55e64a0280eb
UnicycleUnicorn <brand227796@gmail.com>
2024-08-09 14:59:10 -0400

    simplify program.cs

--- Notes Application/Program.cs
+++ Notes Application/Program.cs
@@ -13,56 +13,10 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplicationUtils.Initialize(args);
 
-        // Add services to the container.
+        WebApplicationUtils.AddRemoteCam(builder, "notes");
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        _ = builder.Services.AddSwaggerGen(config => config.OperationFilter<SwaggerAuth>());
-
-        _ = builder.Services.AddAuthentication(NullAuthenticationHandler.RegisterWithBuilder);
-
-        _ = builder.Services.AddExceptionHandler<UuidExceptionHandler>();
-        _ = builder.Services.AddProblemDetails();
-
-        const string CorsAllowAll = "CorsAllowAll";
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy(CorsAllowAll, builder =>
-            {
-                _ = builder.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithExposedHeaders(HeaderUtils.XExceptionCode);
-            });
-        });
-
-        ICamInterface camService = new RemoteCamInterface("notes", "http://cam:8080");
-        //ICamInterface camService = new RemoteCamInterface("notes", "http://localhost:5048");
-        _ = builder.Services.AddSingleton(typeof(ICamInterface), camService);
-
-        var app = builder.Build();
-
-        RuntimePrechecker.RunPrecheck(app);
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        // We don't need https because our services are secured via nginx https through a proxy pass over http to this service
-        // app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.UseCors(CorsAllowAll);
-        
-        _ = app.UseExceptionHandler();
-        app.MapControllers();
-
-        await camService.Initialize();
-
-        app.Run();
+        await WebApplicationUtils.Start(builder);
     }
 }
